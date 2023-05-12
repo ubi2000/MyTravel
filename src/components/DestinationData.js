84e29090bcabacdd7a19066feca00d05b622f00c
ubi2000 <obaidnabi1991@gmail.com>
2023-05-12 23:32:52 +0530

    home complete

--- src/components/DestinationData.js
+++ src/components/DestinationData.js
@@ -1,10 +1,23 @@
 import { Component } from "react";
-import "./Destination.css"
+import "./Destination.css";
 
 
-class DestinationData extends Component{
-    render()
-    return(
-
-    )
+class DestinationData extends Component {
+  render() {
+    return (
+      <div className={this.props.className}>
+        <div className="des-text">
+          <h2>{this.props.heading}</h2>
+          <p>
+            {this.props.text}
+          </p>
+        </div>
+        <div className="image">
+          <img alt="first image " src={this.props.img1} />
+          <img alt="2 image " src={this.props.img2} />
+        </div>
+      </div>
+    );
+  }
 }
+export default DestinationData;
